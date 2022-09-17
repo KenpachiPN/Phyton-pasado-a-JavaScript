a88dfa1b9c3c629c7773a8a75eb978194559a5cc
KenpachiPN <sprada71@gmail.com>
2022-09-17 10:41:39 -0500

    Ejercicio #11

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let a = Number(prompt("Ingrese el valor de a"));
 let b = Number(prompt("Ingrese el valor de b")); 
 let c = Number(prompt("Ingrese el valor de c"));
-let x = 4;
-let result = a * Math.pow(x,2) + b * x + c;
-    alert("El resultado de la ecuación de segundo grado es de: " + result );
+let volumen = (4/3) * Math.PI * a * b * c ;
+    alert("El volumen del elipsoide es de: " + volumen );
 
