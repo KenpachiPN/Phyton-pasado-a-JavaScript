e767e2f29175e228bce16d3c0c8380b90c9d3e18
KenpachiPN <sprada71@gmail.com>
2022-09-17 08:58:33 -0500

    Ejercicio #2

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-let num1 = Number(prompt("Ingrese un número"));
-    let num2 = Number(prompt("Ingrese un número"));
-    let result = num1 + num2;
-    let result1 = num1 - num2;
-    let result2 = num1 * num2;
-    let result3 = num1 / num2;
-    alert("La suma de " + num1 + " y " + num2 + " es de: " + result + "\n" +
-                   "La resta de " + num1 + " y " + num2 + " es de: " + result1 + "\n" +
-                   "La multiplicación de " + num1 + " y " + num2 + " es de: " + result2 + "\n" +
-                   "La división de " + num1 + " y " + num2 + " es de: " + result3);
-
+let lado1 = Number(prompt("Ingrese el primer lado del rectangulo"));
+    let lado2 = Number(prompt("Ingrese el segundo lado del rectangulo"));
+    let area = lado1 + lado2;
+    alert("El área del rectangulo es de: " + area );
                    
