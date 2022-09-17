3a8fc6f3109382b948b9e482456fcf70cdee6f41
KenpachiPN <sprada71@gmail.com>
2022-09-17 12:48:04 -0500

    Ejercicio #19

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let name = prompt("Ingrese su nombre");
-let hortrab = Number(prompt("Ingrese las horas que trabajó"));
-let paghor = 15.300;
-let sueldo = hortrab * paghor;
-    alert("El sueldo de " + name + " por trabajar " + hortrab + " horas, es de: $" + sueldo + " mil pesos" );
+let examen1 = Number(prompt("Ingrese la calificación del primer exámen"));
+let examen2 = Number(prompt("Ingrese la calificación del segundo exámen"));
+let examen3 = Number(prompt("Ingrese la calificación del tercer exámen"));
+let examen4 = Number(prompt("Ingrese la calificación del cuarto exámen"));
+let promedio = (examen1 + examen2 + examen3 + examen4) /4;
+    alert("El promedio de " + name + " en sus 4 exámenes es de: " + promedio + " puntos");
