ffae5bc40ff612fb6f353aee23064ae88d06dbad
KenpachiPN <sprada71@gmail.com>
2022-09-17 09:09:10 -0500

    Ejercicio #4

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-let num1 = Number(prompt("Ingrese el primer número"));
-let num2 = Number(prompt("Ingrese el segundo número"));
-let num3 = Number(prompt("Ingrese el tercer número"));
-let num4 = Number(prompt("Ingrese el cuarto número"));
-    let media = (num1 + num2 + num3 + num4) / 4;
-        alert("La media de los números " + num1 + " , " + num2 + " , " + num3 + " , " + num4 + " es de: " + media );
-                   
+let base = Number(prompt("Ingrese la base del triangulo"));
+let altura = Number(prompt("Ingrese la altura del triangulo"));
+let area = (base * altura) / 2;
+    alert("El área del triangulo es de: " + area );
+                  
