94f9b04ca53cf846e93713bf551840f2ee6cee40
KenpachiPN <sprada71@gmail.com>
2022-09-17 12:21:56 -0500

    Ejercicio #16

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let centigr = Number(prompt("Ingrese los ° centigrados"));
-let result = (centigr * 9/5) + 32;
-    alert("El resultado de convertir °" + centigr + " grados centigrados a fahrenheit es de: °" + result + " grados fahrenheit" );
+let name = prompt("Ingrese su nombre");
+let parcial1 = Number(prompt("Ingrese la calificación del primer parcial"));
+let parcial2 = Number(prompt("Ingrese la calificación del segundo parcial"));
+let parcial3 = Number(prompt("Ingrese la calificación del tercer parcial"));
+let examenfin = Number(prompt("Ingrese la calificación del exámen final"));
+let trabajofin = Number(prompt("Ingrese la calificación del trabajo final"));
+let result = (((parcial1 + parcial2 + parcial3) /3) * 0.55) + (examenfin * 0.30) + (trabajofin * 0.15);
+    alert("El promedio final que tuvo " + name + " en la materia de algoritmos es de: " + result);
